49c789fb61bc99d1f65f66962563809e1d14d983
Austin <austinelliott9@gmail.com>
2025-04-07 19:51:40 -0400

    Added Password generator

---
+++ app/src/main/java/com/example/finalproject448/PasswordGenerator.kt
@@ -0,0 +1,24 @@
+package com.example.finalproject448
+
+class PasswordGenerator {
+    val upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val lowerChars = "abcdefghijklmnopqrstuvwxyz"
+    val numbers = "0123456789"
+    val specialChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+    fun generatePassword(length: Int, useUppercase: Boolean, useLowercase: Boolean, useNumbers: Boolean, useSpecialChars: Boolean): String {
+        var characterPool = ""
+        if (useUppercase) characterPool += upperChars
+        if (useLowercase) characterPool += lowerChars
+        if (useNumbers) characterPool += numbers
+        if (useSpecialChars) characterPool += specialChars
+
+        if (characterPool.isEmpty()) {
+            throw IllegalArgumentException("At least one character type must be selected.")
+        }
+
+        return (1..length)
+            .map { characterPool.random() }
+            .joinToString("")
+    }
+}
