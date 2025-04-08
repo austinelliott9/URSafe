a3a8dff2d4c97c412e37792454976d2c92db9e8d
zaman <zxr126@case.edu>
2025-04-07 21:56:42 -0400

    added dependency for security

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,5 +47,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation(libs.androidx.security.crypto)
 }
